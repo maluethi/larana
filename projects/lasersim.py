9a8fe1237b3d65876ad30646cdb2ace7f332963e
matthias <matthias.luethi@lhep.unibe.ch>
2017-08-22 21:52:02 +0200

    minor rearangements of script

--- projects/lasersim.py
+++ projects/lasersim.py
@@ -1,14 +1,16 @@
 from larana.laser_sim import *
-
+from pprint import pprint
 laser_id = 2
-azimu_start = -1
-azimu_end = 1
-azimu_steps = 3
-polar_start = 89
-polar_end = 91
-polar_steps = 3
+azimu_start = 30
+azimu_end = -5
+azimu_steps = 35
+polar_start = 60
+polar_end = 120
+polar_steps = 30
 
 laser_scan = generate_span(laser_id, azimu_start, azimu_end, azimu_steps, polar_start, polar_end, polar_steps)
 directions = [convert_to_uboone(azi, pol, r, laser_id) for azi, pol, r in laser_scan]
 entry_points = [get_tpc_intersection(LASER_POS[laser_id], direct)[0] for direct in directions]
+pprint([get_tpc_intersection(LASER_POS[laser_id], direct)[1] for direct in directions])
+
 gen_textfile('/home/matthias/workspace/larana/projects/out/input.txt', entry_points, directions)
