69cc453295db3289d0031e372cc0b89429695853
matthias <matthias.luethi@lhep.unibe.ch>
2018-02-12 09:39:35 +0100

    FIX: Faster, removed plotting

--- projects/recalibrate_mirror.py
+++ projects/recalibrate_mirror.py
@@ -9,42 +9,49 @@ laser_filename = "out/laser-data-7267.npy"
 tracks = np.load(tracks_filename)
 lasers = np.load(laser_filename)
 
-NEW_LASER_POS = [102.53, 0, 1077.48]
-CORRECTION_AZIMU = 0.
+NEW_LASER_POS = [102.53, 7.6, 1077.48]
+CORRECTION_AZIMU = 2.
 CORRECTION_POLAR = 0.
 
-fig, ax = laru.make_figure()
+
+
+#fig, ax = laru.make_figure()
 for idx, (laser, track) in enumerate(zip(lasers, tracks)):
     laser_entry, laser_exit, dir, pos, evt = laru.disassemble_laser(laser)
     current_azimuth = np.arctan(dir.x/dir.z)
     current_polar = np.pi/2 - np.arctan(dir.y/np.sqrt(np.power(dir.z, 2) + np.power(dir.x, 2)))
 
-    plt.plot(evt, np.rad2deg(current_azimuth), 'x')
+    #plt.plot(evt, np.rad2deg(current_azimuth), 'x')
 
     new_azimuth = current_azimuth + np.deg2rad(CORRECTION_AZIMU)
     new_polar = current_polar + np.deg2rad(CORRECTION_POLAR)
 
-    new_x = 1.0 * np.sign(dir.x)
-    new_z = new_x / np.tan(new_azimuth)
-    new_y = - np.tan(new_polar - np.pi/2) * np.sqrt(np.power(new_z,2) + np.power(new_x,2))
+    new_laser_dir = np.rec.array([laser['dir.x()'],
+                              laser['dir.y()'],
+                              laser['dir.z()']],
+                             dtype=[('x', 'f'), ('y', 'f'), ('z', 'f')])
 
-    l = np.sqrt(np.sum([np.power(e,2) for e in [new_x, new_y, new_z]]))
-    new_x /= l
-    new_y /= l
-    new_z /= l
+    new_laser_dir.x = 1.0 * np.sign(dir.x)
+    new_laser_dir.z = new_laser_dir.x / np.tan(new_azimuth)
+    new_laser_dir.y = - np.tan(new_polar - np.pi/2) * np.sqrt(np.power(new_laser_dir.z,2) + np.power(new_laser_dir.x,2))
 
-    en, ex = geom.get_tpc_intersection(NEW_LASER_POS, [new_x, new_y, new_z])
+    l = np.sqrt( np.sum(np.power(new_laser_dir.tolist(), 2)))
+    new_laser_dir.x /= l
+    new_laser_dir.y /= l
+    new_laser_dir.z /= l
 
-    print([new_x, new_y, new_z], dir)
+    en, ex = geom.get_tpc_intersection(NEW_LASER_POS, new_laser_dir.tolist())
 
-    print(dir.x - new_x)
+    #print(new_laser_dir, dir)
 
-    laru.plot_edges(ax, en, ex, linestyle='--', color='b', marker='x', alpha=.1)
-    laru.plot_edges(ax, laser_entry.tolist(), laser_exit.tolist(), linestyle='--', color='g', marker='x', alpha=.1)
+    #print(dir.x - new_laser_dir.x)
 
-    if idx % 100 == 0:
+    #laru.plot_edges(ax, en, ex, linestyle='--', color='b', marker='x', alpha=.1)
+    #laru.plot_edges(ax, laser_entry.tolist(), laser_exit.tolist(), linestyle='--', color='g', marker='x', alpha=.1)
+
+    if evt % 100000== 0:
         plt.show()
         fig, ax = laru.make_figure()
 
-plt.show()
+#plt.show()
 
