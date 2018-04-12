69b4a5449890b6ae31b34b6adfe543e8bad1b84d
matthias <matthias.luethi@lhep.unibe.ch>
2018-04-12 10:56:15 +0200

    ADD: gauss fit

--- projects/time_analysis.py
+++ projects/time_analysis.py
@@ -1,9 +1,28 @@
 from larana import lar_utils as laru
 import numpy as np
-
 import glob
-
 import multiprocessing as mp
+from lmfit import Model, Parameters
+from lmfit.models import GaussianModel
+
+import matplotlib.pyplot as plt
+
+def gaussian(x, amp, cen, wid):
+    "1-d gaussian: gaussian(x, amp, cen, wid)"
+    return (amp/(np.sqrt(2*np.pi)*wid)) * np.exp(-(x-cen)**2 /(2*wid**2))
+
+def get_gauss_fit(x, y, center_guess):
+    gmodel = Model(gaussian)
+    params = Parameters()
+    params.add('amp', value=50, min=5, max=np.inf)
+    params.add('cen', value=center_guess, min=-250, max=250)
+    params.add('wid', value=50)
+
+    mod = GaussianModel()
+    pars = mod.guess(y, x=x)
+    out = mod.fit(y, pars, x=x)
+
+    return out
 
 
 def get_histo(in_file):
@@ -12,9 +31,9 @@ def get_histo(in_file):
 
     event_ids = np.unique([wire[0] for wire in raw])
     #wire_ids = np.unique([wire[2] for wire in raw])
-
+    previous_event = event_ids[0]
     maxima = np.zeros([3456, len(event_ids)])
-
+    ticks = np.arange(0, len(raw[0][4]))
     for wire in raw:
         wire_id = wire[2]
         event = wire[0]
@@ -23,13 +42,29 @@ def get_histo(in_file):
         raw_digits = wire[4] - baseline
         maxima[wire_id, event-1 - event_ids[0]] = np.argmax(raw_digits)
 
+        res = get_gauss_fit(ticks, raw_digits, np.argmax(raw_digits))
+        amp = res.params['amplitude']
+        cen = res.params['center']
+        wid = res.params['sigma']
+
+        if (previous_event != event):
+            print("Processing: {}/{}".format(event, event_ids[-1]))
+
+        previous_event = event
+
+
     return maxima
 
-file_path = '/home/data/uboone/laser/7275/'
-in_files = glob.glob(file_path + 'Rw*')
+file_path = '/home/data/uboone/laser/7275/rwa/'
+in_files = glob.glob(file_path + 'Rw*[0-3][0-9]*')
+
+in_files = '/home/data/uboone/laser/7275/rwa/RwaData-7275-006.root'
 print(in_files)
 
-pool = mp.Pool(processes=2)
-res = pool.map(get_histo, in_files)
-np.save('out/time-histo/histo-1.npy', res)
-print("saved file to: 'out/time-histo/histo-1.npy'")
+#pool = mp.Pool(processes=1)
+#res = pool.map(get_histo, in_files)
+
+res = get_histo(in_files)
+print(res)
+np.save('out/time-histo/histo-more-test.npy', res)
+print("saved file to: 'out/time-histo/histo-more-test.npy'")
